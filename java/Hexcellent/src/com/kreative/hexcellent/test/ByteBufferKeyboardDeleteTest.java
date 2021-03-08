e52d44b6710d1ffb6e76e9ed0cc253318211d469
Rebecca G. Bettencourt <beckiejo@gmail.com>
2021-03-08 12:21:12 -0800

    Extract base class from tests

--- java/Hexcellent/src/com/kreative/hexcellent/test/ByteBufferKeyboardDeleteTest.java
+++ java/Hexcellent/src/com/kreative/hexcellent/test/ByteBufferKeyboardDeleteTest.java
@@ -1,51 +1,11 @@
 package com.kreative.hexcellent.test;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import com.kreative.hexcellent.buffer.ArrayByteBuffer;
 import com.kreative.hexcellent.buffer.ByteBuffer;
 import com.kreative.hexcellent.buffer.ByteBufferDocument;
-import com.kreative.hexcellent.buffer.CompositeByteBuffer;
-import com.kreative.hexcellent.buffer.RafByteBuffer;
 
-public class ByteBufferKeyboardDeleteTest {
-	private static final Random random = new Random();
-	
-	private static ByteBuffer randomByteBuffer(int start, int end, boolean editable) throws IOException {
-		switch (random.nextInt(3)) {
-			case 0:
-				int fileStart = random.nextInt(start + 1);
-				int fileEnd = end + random.nextInt(257 - end);
-				File file = File.createTempFile("ByteBufferReadTest", ".bin");
-				FileOutputStream out = new FileOutputStream(file);
-				for (int i = fileStart; i < fileEnd; i++) out.write(i);
-				out.flush();
-				out.close();
-				file.deleteOnExit();
-				RafByteBuffer raf = new RafByteBuffer(file, start - fileStart, end - start);
-				return editable ? new CompositeByteBuffer(raf) : raf;
-			case 1:
-				int arrStart = random.nextInt(start + 1);
-				int arrEnd = end + random.nextInt(257 - end);
-				byte[] arr = new byte[arrEnd - arrStart];
-				for (int i = 0, v = arrStart; v < arrEnd; v++, i++) arr[i] = (byte)v;
-				return new ArrayByteBuffer(arr, start - arrStart, end - start);
-			default:
-				List<ByteBuffer> buffers = new ArrayList<ByteBuffer>();
-				while (start < end) {
-					int bufEnd = start + random.nextInt(end - start + 1);
-					buffers.add(randomByteBuffer(start, bufEnd, false));
-					start = bufEnd;
-				}
-				return new CompositeByteBuffer(buffers);
-		}
-	}
-	
+public class ByteBufferKeyboardDeleteTest extends ByteBufferTestBase {
 	private static void testDelete() throws IOException {
 		ByteBuffer b = randomByteBuffer(0, 256, true);
 		ByteBufferDocument d = new ByteBufferDocument(b);
